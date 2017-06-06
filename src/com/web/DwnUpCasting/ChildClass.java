259d1698bd4680ef46a767cf1845f04371b114fe
ashaj12 <ashjyo81064>
2017-06-06 09:54:26 +0530

    @javabasics narendra

---
+++ src/com/web/DwnUpCasting/ChildClass.java
@@ -0,0 +1,42 @@
+package com.web.DwnUpCasting;
+
+public class ChildClass extends ParentClass{
+	
+	public void mc1(){
+		System.out.println("child class method");
+	}
+	public void mc2(){
+		System.out.println("child class method2");
+	}
+
+	public static void main(String[] args) {
+		
+		ChildClass c=new ChildClass();
+		ParentClass p=new ParentClass();
+		c.mc1();
+		p.mp1();
+		
+		
+		//upcasting 
+		ParentClass p1=new ChildClass();
+		p1.mp1();
+		
+	/*	//down casting
+		ParentClass p2=new ChildClass();
+		ChildClass c2=(ChildClass) p2;
+		c2.m2();
+		
+		
+	*/	
+	
+	
+		
+		
+	
+		
+		
+		
+		
+	}
+	
+}
---
+++ src/com/web/DwnUpCasting/ParentClass.java
@@ -0,0 +1,9 @@
+package com.web.DwnUpCasting;
+
+public class ParentClass {
+	
+	public void mp1(){
+		System.out.println("parent method");
+	}
+
+}
